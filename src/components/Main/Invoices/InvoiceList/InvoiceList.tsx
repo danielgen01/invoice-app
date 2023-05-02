d4202a0581191a2efad3b1bbfa15ef45fbbaf651
Daniel <gendik38@gmail.com>
2023-05-02 18:54:17 +0200

    map through data.json

--- src/components/Main/Invoices/InvoiceList/InvoiceList.tsx
+++ src/components/Main/Invoices/InvoiceList/InvoiceList.tsx
@@ -1,27 +1,24 @@
 import React from "react"
 import InvoiceRow from "../../../Reusable/InvoiceRow"
 import { RootState } from "../../../../Redux/rootReducer"
-import { useAppDispatch,useAppSelector } from "../../../../Redux/store"
+import { useAppDispatch, useAppSelector } from "../../../../Redux/store"
 
 const InvoiceList = () => {
+  const data = useAppSelector((state: RootState) => state.data.Data)
 
-    const data = useAppSelector((state: RootState) => state.data);
+  console.log(data)
 
   return (
     <section className="invoice-list rows flex flex-col items-center mt-10 gap-6">
-        <InvoiceRow />
-        <InvoiceRow />
-
-        <InvoiceRow />
-
-        <InvoiceRow />
-        <InvoiceRow />
-        <InvoiceRow />
-        <InvoiceRow />
-        <InvoiceRow />
-        <InvoiceRow />
-
-    
+      {data.map((invoice) => (
+        <InvoiceRow key={invoice.id}
+         invoiceId={invoice.id}
+         paymentDue={invoice.paymentDue}
+         totalAmount={invoice.total}
+         clientName={invoice.clientName}
+         status={invoice.status}
+         />
+      ))}
     </section>
   )
 }
--- src/components/Reusable/InvoiceRow.tsx
+++ src/components/Reusable/InvoiceRow.tsx
@@ -1,6 +1,20 @@
 import React from "react"
 
-const InvoiceRow = () => {
+type props = {
+  invoiceId: string
+  paymentDue: string
+  totalAmount: number
+  clientName: string
+  status: string
+}
+
+const InvoiceRow: React.FC<props> = ({
+  invoiceId,
+  paymentDue,
+  totalAmount,
+  clientName,
+  status,
+}) => {
   return (
     <>
       <div className="row h-40 bg-white shadow-lg rounded-lg dark:bg-[#1E2139]  w-[90%]">
@@ -9,19 +23,20 @@ const InvoiceRow = () => {
           <section className="first-grid-item flex flex-col gap-5">
             <h1 className="font-bold uppercase">
               <span className="text-[#7e88C3]">#</span>
-              rrr
+              {invoiceId}
             </h1>
             <p className="text-medium-gray font-bold whitespace-nowrap text-sm">
-              Due 19 August 2021
+              {paymentDue}
             </p>
-            <h2 className="font-bold">€ 2.000,00</h2>
+            <h2 className="font-bold">€ {totalAmount}</h2>
           </section>
           {/* SECOND GRID COL */}
           <section className="second-grid-item flex flex-col items-end gap-10 ">
-            <p className="text-medium-gray font-bold ">Jensen huang</p>
-            <div className="status flex items-center bg-green-500/10 px-10 py-4 rounded-xl gap-2">
+            <p className="text-medium-gray font-bold ">{clientName}</p>
+            <div className="status flex items-center
+             bg-green-500/10 w-28 justify-center py-4 rounded-xl gap-2">
               <div className="status-batch w-3 h-3 bg-green-500 rounded-full"></div>
-              <h1 className="text-green-500 font-bold">Paid</h1>
+              <h1 className="text-green-500 font-bold">{status}</h1>
             </div>
           </section>
         </div>
