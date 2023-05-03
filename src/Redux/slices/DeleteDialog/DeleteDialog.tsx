35f4addc3b4f553e39e1aba274483af93cf465db
Daniel <gendik38@gmail.com>
2023-05-03 08:54:01 +0200

    some dialog style change s

--- src/Redux/slices/DeleteDialog/DeleteDialog.tsx
+++ src/Redux/slices/DeleteDialog/DeleteDialog.tsx
@@ -9,13 +9,14 @@ const DeleteDialog = () => {
       >
         <div
           className="-translate-x-1/2 -translate-y-1/2 bg-white dark:bg-[#252945]
-    w-[90%] h-52 absolute top-1/2 left-1/2 rounded-xl"
+    w-72 md:w-96 xl:w-[25%] h-52 absolute top-1/2 left-1/2 rounded-xl"
         >
           <div className="dialog-content py-8 px-5 w-full">
             <h1 className="font-bold text-2xl dark:text-white">Confirm Deletion</h1>
             <p className="text-medium-gray mt-2">Are you sure you want to dele invoice 
-            <br/> ""? This action cannot be undone.</p>
-            <div className="buttons gap-10 flex flex-row mt-5 pl-auto w-full">
+             ""? This action cannot be undone.</p>
+            <div className="buttons gap-10 flex flex-row xl:mt-5
+             pl-auto w-full justify-end">
               <button className="bg-[#F8F8FB] px-6 py-3 rounded-full
               font-bold text-[#9277FF] dark:bg-[#1E2139] dark:text-[#DFE3FA]">
                 Cancel
