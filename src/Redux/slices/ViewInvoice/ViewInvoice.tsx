c022a88ddb18b01ea4cfe04a857ef24190f8e80b
Daniel <gendik38@gmail.com>
2023-05-04 08:20:47 +0200

    just some design editings

--- src/Redux/slices/ViewInvoice/ViewInvoice.tsx
+++ src/Redux/slices/ViewInvoice/ViewInvoice.tsx
@@ -15,12 +15,12 @@ const ViewInvoice = () => {
           <h1 className="font-bold dark:text-white">Go back</h1>
         </button>
         <StatusBox />
-        <div className="invoice-box bg-white  mt-5 shadow-md rounded-xl ">
+        <div className="invoice-box bg-white  mt-5 shadow-md rounded-xl dark:bg-[#252945]">
           <section className="invoice-details-content md:py-10">
             <TitleAndAdress />
             <InvoiceInfos />
-            <section className="price-overview bg-[#F8F8FB] w-full min-h-[300px] mt-5 rounded-xl">
-              <div className="headline hidden md:grid md:grid-cols-4 px-5">
+            <section className="price-overview bg-[#F8F8FB] w-full min-h-[300px] mt-4 rounded-xl">
+              <div className="headline hidden md:grid md:grid-cols-4 px-5 bg-[#252945] ">
                 <h1 className="font-bold text-medium-gray">Item Name</h1>
                 <h1 className="font-bold text-medium-gray flex justify-center">
                   Qty.
@@ -30,7 +30,7 @@ const ViewInvoice = () => {
                   Total
                 </h1>
               </div>
-              <ul className="content-item-list px-6 py-6 grid grid-cols-2 w-full gap-5 md:grid-cols-4">
+              <ul className="content-item-list px-6 py-6 grid grid-cols-2 w-full gap-5 md:grid-cols-4 dark:bg-[#252945]">
                 <Item />
                 <Item />
                 <Item />
@@ -39,15 +39,15 @@ const ViewInvoice = () => {
             </section>
           </section>
         </div>
-        <footer className="block md:hidden bg-white  mt-20">
-          <div className="footer-content px-5 py-5 flex justify-between">
-            <button className="md:hidden  py-3 px-6 bg-[#F8F8FB] rounded-full text-[#9277FF] font-bold">
+        <footer className="block md:hidden bg-white  mt-20 dark:bg-[#1E2139]">
+          <div className="footer-content px-5 py-5 flex justify-between items-center">
+            <button className="md:hidden  w-20 h-10 bg-[#F8F8FB] rounded-full text-[#9277FF] font-bold dark:bg-[#252945] dark:text-medium-gray">
               Edit
             </button>
-            <button className="md:hidden  py-3 px-6 bg-[#EC5757] rounded-full text-white font-bold">
+            <button className="md:hidden  w-20 h-12 bg-[#EC5757] rounded-full text-white font-bold">
               Delete
             </button>
-            <button className="md:hidden  py-3 px-6  bg-[#7C5DFA] rounded-full text-white font-bold">
+            <button className="md:hidden w-32 h-12  bg-[#7C5DFA] rounded-full text-white font-bold">
               Mark as Paid
             </button>
           </div>
@@ -85,9 +85,9 @@ function StatusBox() {
 
 function TitleAndAdress() {
   return (
-    <section className="invoice-title-and-adress grid grid-cols-1 md:grid-cols-2 w-full gap-5 px-2">
+    <section className="invoice-title-and-adress grid grid-cols-1 md:grid-cols-2 w-full gap-5 px-2 dark:bg-[#252945]">
       <div className="invoice-num-and-name">
-        <h1 className="font-bold ">#XM9124</h1>
+        <h1 className="font-bold  dark:text-white">#XM9124</h1>
         <p className="text-sm text-medium-gray font-bold">Graphic Design</p>
       </div>
       <div className="adress-bill-from flex flex-col md:items-end">
@@ -102,15 +102,15 @@ function TitleAndAdress() {
 
 function InvoiceInfos() {
   return (
-    <section className="grid grid-cols-2 mt-4 gap-2 md:grid-cols-3 px-2">
+    <section className="grid grid-cols-2 pt-4 gap-2 md:grid-cols-3 px-2 dark:bg-[#252945]">
       <div className="invoice-date flex flex-col gap-2">
         <p className="text-sm font-bold text-medium-gray">Invoice Date</p>
-        <h1 className="font-bold ">21 Aug 2021</h1>
+        <h1 className="font-bold dark:text-white">21 Aug 2021</h1>
       </div>
 
       <div className="bill-to flex flex-col ">
         <p className="text-sm font-bold text-medium-gray">Bill to</p>
-        <h1 className="font-bold ">Alex Grim</h1>
+        <h1 className="font-bold  dark:text-white">Alex Grim</h1>
         <p className="text-sm text-medium-gray font-bold">84 Church Way</p>
         <p className="text-sm text-medium-gray font-bold">Brodford</p>
         <p className="text-sm text-medium-gray font-bold">BD 32 9FB</p>
@@ -118,11 +118,11 @@ function InvoiceInfos() {
       </div>
       <div className="payment-due flex flex-col gap-2 col-span-2 md:col-span-1 md:order-4">
         <p className="text-sm font-bold text-medium-gray">Payment Due</p>
-        <h1 className="font-bold ">21 Sep 2021</h1>
+        <h1 className="font-bold  dark:text-white">21 Sep 2021</h1>
       </div>
       <div className="sent-to flex flex-col gap-2">
         <p className="text-sm font-bold text-medium-gray">Sent to</p>
-        <h1 className="font-bold ">alexgrim@gmail.com</h1>
+        <h1 className="font-bold  dark:text-white">alexgrim@gmail.com</h1>
       </div>
     </section>
   )
@@ -133,7 +133,7 @@ function Item() {
     <>
       {" "}
       <div className="flex flex-col gap-2 ">
-        <h1 className="font-bold">Banner Design</h1>
+        <h1 className="font-bold dark:text-white">Banner Design</h1>
         <p className="font-bold text-medium-gray text-sm md:hidden">
           1 x € 156
         </p>
@@ -142,10 +142,10 @@ function Item() {
         <h1 className="font-bold text-medium-gray text-lg">1</h1>
       </div>
       <div className="price hidden md:block ">
-        <h1 className="font-bold text-medium-gray text-lg">€ 156.00</h1>
+        <h1 className="font-bold text-medium-gray text-lg ">€ 156.00</h1>
       </div>
       <div className="flex justify-end">
-        <h1 className="font-bold">€ 156.00</h1>
+        <h1 className="font-bold dark:text-white">€ 156.00</h1>
       </div>
     </>
   )
@@ -153,7 +153,7 @@ function Item() {
 
 function TotalBox() {
   return (
-    <section className="total-box min-h-[80px] bg-[#1E2139] rounded-b-xl">
+    <section className="total-box min-h-[80px] bg-[#1E2139] rounded-b-xl dark:bg-black">
       <div className="total-content py-10 px-10 grid grid-cols-2 text-white ">
         <div className="total-title">
           <h1>Grand Total</h1>
