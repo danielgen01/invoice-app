f954810a55da123ad45b6d40b2165e676f3de367
Daniel <gendik38@gmail.com>
2023-05-04 07:56:37 +0200

    style invoice box

--- src/Redux/slices/ViewInvoice/ViewInvoice.tsx
+++ src/Redux/slices/ViewInvoice/ViewInvoice.tsx
@@ -1,11 +1,12 @@
 import React from "react"
 import arrowleft from "../../../../public/assets/icon-arrow-left.svg"
 import { BsCircleFill } from "react-icons/bs"
+
 const ViewInvoice = () => {
   return (
     <>
       <div className="overlay"> </div>
-      <main className="content px-5 py-5">
+      <main className="content px-5 py-5 xl:px-72 lg:py-10">
         <button
           className="go-back-button flex items-center gap-5 "
           //   onClick={handleToggleNewInvoiceForm}
@@ -14,39 +15,27 @@ const ViewInvoice = () => {
           <h1 className="font-bold dark:text-white">Go back</h1>
         </button>
         <StatusBox />
-        <div className="invoice-box bg-white min-h-[300px] mt-5 shadow-md rounded-xl">
-          <div className="invoice-details-content px-5 py-5">
+        <div className="invoice-box bg-white  mt-5 shadow-md rounded-xl ">
+          <div className="invoice-details-content md:py-10">
             <TitleAndAdress />
-            <section className="grid grid-cols-2 mt-4 gap-2 md:grid-cols-3">
-              <div className="invoice-date flex flex-col gap-2">
-                <p className="text-sm font-bold text-medium-gray">
-                  Invoice Date
-                </p>
-                <h1 className="font-bold ">21 Aug 2021</h1>
+            <InvoiceInfos />
+            <section className="price-overview bg-[#F8F8FB] w-full min-h-[300px] mt-5 rounded-xl">
+              <div className="headline hidden md:grid md:grid-cols-4 px-5">
+                <h1 className="font-bold text-medium-gray">Item Name</h1>
+                <h1 className="font-bold text-medium-gray flex justify-center">
+                  Qty.
+                </h1>
+                <h1 className="font-bold text-medium-gray ml-7">Price</h1>
+                <h1 className="flex justify-end font-bold text-medium-gray">
+                  Total
+                </h1>
               </div>
-
-              <div className="bill-to flex flex-col ">
-                <p className="text-sm font-bold text-medium-gray">Bill to</p>
-                <h1 className="font-bold ">Alex Grim</h1>
-                <p className="text-sm text-medium-gray font-bold">
-                  84 Church Way
-                </p>
-                <p className="text-sm text-medium-gray font-bold">Brodford</p>
-                <p className="text-sm text-medium-gray font-bold">BD 32 9FB</p>
-                <p className="text-sm text-medium-gray font-bold">
-                  United Kingdom
-                </p>
-              </div>
-              <div className="payment-due flex flex-col gap-2 col-span-2 md:col-span-1 md:order-4">
-                <p className="text-sm font-bold text-medium-gray">
-                  Payment Due
-                </p>
-                <h1 className="font-bold ">21 Sep 2021</h1>
-              </div>
-              <div className="sent-to flex flex-col gap-2">
-                <p className="text-sm font-bold text-medium-gray">Sent to</p>
-                <h1 className="font-bold ">alexgrim@gmail.com</h1>
-              </div>
+              <ul className="content-item-list px-6 py-6 grid grid-cols-2 w-full gap-5 md:grid-cols-4">
+                <Item />
+                <Item />
+                <Item />
+              </ul>
+              <TotalBox />
             </section>
           </div>
         </div>
@@ -83,7 +72,7 @@ function StatusBox() {
 
 function TitleAndAdress() {
   return (
-    <section className="invoice-title-and-adress grid grid-cols-1 md:grid-cols-2 w-full gap-5">
+    <section className="invoice-title-and-adress grid grid-cols-1 md:grid-cols-2 w-full gap-5 px-2">
       <div className="invoice-num-and-name">
         <h1 className="font-bold ">#XM9124</h1>
         <p className="text-sm text-medium-gray font-bold">Graphic Design</p>
@@ -93,6 +82,72 @@ function TitleAndAdress() {
         <p className="text-sm text-medium-gray font-bold">London</p>
         <p className="text-sm text-medium-gray font-bold">E1 3EZ</p>
         <p className="text-sm text-medium-gray font-bold">United Kingdom</p>
+      </div>
+    </section>
+  )
+}
+
+function InvoiceInfos() {
+  return (
+    <section className="grid grid-cols-2 mt-4 gap-2 md:grid-cols-3 px-2">
+      <div className="invoice-date flex flex-col gap-2">
+        <p className="text-sm font-bold text-medium-gray">Invoice Date</p>
+        <h1 className="font-bold ">21 Aug 2021</h1>
+      </div>
+
+      <div className="bill-to flex flex-col ">
+        <p className="text-sm font-bold text-medium-gray">Bill to</p>
+        <h1 className="font-bold ">Alex Grim</h1>
+        <p className="text-sm text-medium-gray font-bold">84 Church Way</p>
+        <p className="text-sm text-medium-gray font-bold">Brodford</p>
+        <p className="text-sm text-medium-gray font-bold">BD 32 9FB</p>
+        <p className="text-sm text-medium-gray font-bold">United Kingdom</p>
+      </div>
+      <div className="payment-due flex flex-col gap-2 col-span-2 md:col-span-1 md:order-4">
+        <p className="text-sm font-bold text-medium-gray">Payment Due</p>
+        <h1 className="font-bold ">21 Sep 2021</h1>
+      </div>
+      <div className="sent-to flex flex-col gap-2">
+        <p className="text-sm font-bold text-medium-gray">Sent to</p>
+        <h1 className="font-bold ">alexgrim@gmail.com</h1>
+      </div>
+    </section>
+  )
+}
+
+function Item() {
+  return (
+    <>
+      {" "}
+      <div className="flex flex-col gap-2 ">
+        <h1 className="font-bold">Banner Design</h1>
+        <p className="font-bold text-medium-gray text-sm md:hidden">
+          1 x € 156
+        </p>
+      </div>
+      <div className="quantity hidden md:flex justify-center">
+        <h1 className="font-bold text-medium-gray text-lg">1</h1>
+      </div>
+      <div className="price hidden md:block ">
+        <h1 className="font-bold text-medium-gray text-lg">€ 156.00</h1>
+      </div>
+      <div className="flex justify-end">
+        <h1 className="font-bold">€ 156.00</h1>
+      </div>
+    </>
+  )
+}
+
+function TotalBox() {
+  return (
+    <section className="total-box min-h-[80px] bg-[#1E2139] rounded-b-xl">
+      <div className="total-content py-10 px-10 grid grid-cols-2 text-white ">
+        <div className="total-title">
+          <h1>Grand Total</h1>
+        </div>
+        <div className="total-sum flex justify-end  font-bold">
+          <h1 className="text-xl">€ 0.00</h1>
+        </div>
       </div>
     </section>
   )
