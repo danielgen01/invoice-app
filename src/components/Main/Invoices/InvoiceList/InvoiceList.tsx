0c32f82a105205f963e4d1c5ce9e61239f111199
Daniel <gendik38@gmail.com>
2023-05-07 16:30:52 +0200

    render emptypage if no invoices

--- src/components/Main/Invoices/InvoiceList/InvoiceList.tsx
+++ src/components/Main/Invoices/InvoiceList/InvoiceList.tsx
@@ -3,6 +3,7 @@ import InvoiceRow from "../../../Reusable/InvoiceRow"
 import { RootState } from "../../../../Redux/rootReducer"
 import { useAppDispatch, useAppSelector } from "../../../../Redux/store"
 import { resetFilter } from "../../../../Redux/slices/Data/DataSlice"
+import EmptyBoard from "../EmptyBoardContent/EmptyBoard"
 
 const InvoiceList = () => {
   const dispatch = useAppDispatch()
@@ -15,20 +16,23 @@ const InvoiceList = () => {
     dispatch(resetFilter())
   }, [data, dispatch])
 
-  console.log(data)
 
   return (
     <section className="invoice-list rows flex flex-col items-center mt-10 gap-6">
-      {filteredData.map((invoice) => (
-        <InvoiceRow
-          key={invoice.id}
-          invoiceId={invoice.id}
-          paymentDue={invoice.paymentDue}
-          totalAmount={invoice.total}
-          clientName={invoice.clientName}
-          status={invoice.status}
-        />
-      ))}
+      {filteredData.length === 0 ? (
+        <EmptyBoard />
+      ) : (
+        filteredData.map((invoice) => (
+          <InvoiceRow
+            key={invoice.id}
+            invoiceId={invoice.id}
+            paymentDue={invoice.paymentDue}
+            totalAmount={invoice.total}
+            clientName={invoice.clientName}
+            status={invoice.status}
+          />
+        ))
+      )}
     </section>
   )
 }
